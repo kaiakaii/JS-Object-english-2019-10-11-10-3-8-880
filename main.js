e35cb2b0927b615ae18f9c8d1177c7504c435c97
sky <senkk512@163.com>
2020-06-19 17:42:27 +0800

    first

---
+++ main.js
@@ -0,0 +1,14 @@
+/*1.*/
+var user = {
+    name:"John",
+    surname:"Mike",
+}
+user.name = "Peter";
+delete user.surname;
+/*2.*/
+var fruit = {
+    apple: 20,
+    pear: 20,
+    peach: 10
+};
+console.log(fruit.apple+fruit.peach+fruit.pear)
